6e678e5036a31e264bb9d78752695dd67942bf9d
Josh Goldberg <josh@fullscreenmario.com>
2019-11-21 09:29:47 -0500

    Added budgie dependency; initial version

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
-export default async (name = 'World'): Promise<string> => {
-  return `Hello ${name}!`
+import { Budgie } from "budgie";
+
+export type BudgifySettings = {
+  language: string;
+  text: string;
+}
+
+export default async ({ language, text }: BudgifySettings): Promise<string[]> => {
+  return new Budgie(language).convert(text.split(/\r\n|\r|\n/));
 }
